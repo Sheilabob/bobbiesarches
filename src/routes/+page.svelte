68afd09450a26b2d79e0a581eb4e79b39b5fd723
Sheila <smstrahan@yahoo.com>
2023-03-21 18:53:59 -0600

    repositioned label

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,23 +11,13 @@
 </svelte:head>
 
 <section>
-  <h1>
-    <span class="welcome">
-      <picture>
-        <source srcset={welcome} type="image/webp" />
-        <img src={welcome_fallback} alt="Welcome" />
-      </picture>
-    </span>
-    <span class="coronaarch">
-      <picture>
-        <source srcset={coronaarch} type="image/jpg" />
-        <img src={coronaarch} alt="Corona Arch with Claret Cup in front" />
-      </picture>
-    </span>
+  <span class="coronaarch">
+    <picture>
+      <source srcset={coronaarch} type="image/jpg" />
+      <img src={coronaarch} alt="Corona Arch with Claret Cup in front" />
+    </picture>
     <div class="piclabel">Corona Arch</div>
-
-    to my new<br />SvelteKit app
-  </h1>
+  </span>
 
   <h2>
     try editing <strong>src/routes/+page.svelte</strong>
@@ -45,42 +35,18 @@
     flex: 0.6;
   }
 
-  h1 {
-    width: 100%;
-  }
-
-  .welcome {
-    display: block;
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
-
   .coronaarch {
     display: inline-block;
     position: relative;
     width: 25%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
   }
 
   .coronaarch img {
-    position: absolute;
     width: 100%;
-    top: 0;
-    display: inline-block;
+    padding: 0;
   }
 
   .piclabel {
-    margin-top: 50px;
+    margin-top: 5px;
   }
 </style>
