93f1d9f5383876d96e852b671c73eeffd487ebf2
Sheila <smstrahan@yahoo.com>
2023-03-25 18:40:02 -0600

    added angel arch photo card

--- src/routes/PictureCard.svelte
+++ src/routes/PictureCard.svelte
@@ -1,24 +1,38 @@
 <script lang="ts">
   import coronaarch from "$lib/images/corona-arch.jpg";
+  import angelarch from "$lib/images/angel-arch.jpeg";
 </script>
 
-<div class="coronaarch card">
-  <picture>
-    <source srcset={coronaarch} type="image/jpg" />
-    <img src={coronaarch} alt="Corona Arch with Claret Cup in front" />
-  </picture>
-  <div class="piclabel">Corona Arch</div>
-</div>
-<div class="card">
-  <h2>Corona Arch Info</h2>
-  <p>Size: 110ft x 110ft</p>
-  <p>Type: Buttress</p>
-  <p>Date of Picture: 4/24/22</p>
-  <p>Trail Description: Well-marked, busy trail near Moab, UT.</p>
-  <p>GPS: 38 34.790' -109 37.197'</p>
+<div class="piccards">
+  <div class="coronaarch card">
+    <picture>
+      <source srcset={coronaarch} type="image/jpg" />
+      <img src={coronaarch} alt="Corona Arch with Claret Cup in front" />
+    </picture>
+    <div class="piclabel">Corona Arch</div>
+  </div>
+  <div class="card">
+    <h2>Corona Arch Info</h2>
+    <p>Size: 110ft x 110ft</p>
+    <p>Type: Buttress</p>
+    <p>Date of Picture: 4/24/22</p>
+    <p>Trail Description: Well-marked, busy trail near Moab, UT.</p>
+    <p>GPS: 38 34.790' -109 37.197'</p>
+  </div>
+  <div class="coronaarch card">
+    <picture>
+      <source srcset={angelarch} type="image/jpg" />
+      <img src={angelarch} alt="Columbine with Angel Arch in the background" />
+    </picture>
+    <div class="piclabel">Angel Arch</div>
+  </div>
 </div>
 
 <style>
+  .piccards {
+    display: flex;
+  }
+
   .coronaarch {
     display: inline-block;
     position: relative;
